786638cf1d6235089d43467dbe098c85e7394d70
daddydemir <42716195+daddydemir@users.noreply.github.com>
2025-01-02 22:57:58 +0300

    del: unnecessary files removed.

--- internal/generator/main.go
+++
@@ -1,9 +0,0 @@
-package generator
-
-import "github.com/daddydemir/RAJ/internal/models"
-
-type Generate interface {
-	GenerateMySQL(model models.Object) string
-	GenerateOracleDB(model models.Object) string
-	GenerateSQLServer(model models.Object) string
-}
